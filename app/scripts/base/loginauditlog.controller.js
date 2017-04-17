920a343ad32c9d42248e401d0f16262b1f45ba43
Matias Hapanowicz <mhapanow@hotmail.com>
2017-04-17 14:28:21 -0500

    https://app.asana.com/0/258232522202675/315701342429889

--- app/scripts/base/loginauditlog.controller.js
+++ app/scripts/base/loginauditlog.controller.js
@@ -36,7 +36,7 @@ function LoginAuditLogCtrl($scope, $http, $location, $stateParams, CommonsServic
 		$http.get(CommonsService.getUrl('/auditlog') 
 			+ '&order=key&from=0&to=10&status=0&eventType=0&userId=' + encodeURIComponent($scope.userId)
 			+ '&q=' + encodeURIComponent($scope.search))
-			.then($scope.fillTableActive);
+			.then($scope.fillTable);
 	}
 
 	$scope.fillRecord = function(obj, status) {
@@ -50,8 +50,61 @@ function LoginAuditLogCtrl($scope, $http, $location, $stateParams, CommonsServic
 	    return row;
 	}
 
-	$scope.fillTableActive = function(data) {
-		CommonsService.paginateCommonTable($scope, 'auditlog', data, 0, null);
+	$scope.fillTable = function(data) {
+
+		var assignedCount = 0;
+
+		//get the footable object
+		var tableAssigned = $('#auditlog-table').data('footable');
+		tableAssigned.pageCallback = function(ft, pageNumber, sort, callback) {
+			var $table = $(ft.table), data = $table.data();
+			var pageSize = data.pageSize || ft.options.pageSize;
+			var from = pageSize * pageNumber;
+			var to = from + pageSize;
+
+			$scope.loadingRefresh = true;
+			$scope.status.httpCallback = callback;
+			$scope.status.ft = ft;
+			$scope.status.pageNumber = pageNumber;
+
+			$http.get(CommonsService.getUrl('/auditlog')  
+				+ '&from=' + from 
+				+ '&to=' + to 
+				+ '&eventType=0'
+				+ '&userId=' + encodeURIComponent($scope.userId))
+				.then($scope.fillTable);
+		}
+
+	    $("#auditlog-table>tbody>tr").each(function(index, elem){$(elem).remove();});
+
+	    tableAssignedRows = '';
+
+	    for(var i = 0; i < data.data.data.length; i++) {
+	    	var obj = data.data.data[i];
+	    	var newRow = $scope.fillRecord(obj, null);
+    		tableAssignedRows += newRow;
+    		assignedCount++;
+	    }
+
+	    tableAssigned.appendRow(tableAssignedRows);
+
+		$('#auditlog-table').data('current-page', '0');
+		$('#auditlog-table').data('record-count', data.data.recordCount);
+
+		tableAssigned.redraw();
+
+    	$("#auditlog-count").html('&nbsp;(' + data.data.recordCount + ')');
+
+	    if( $scope.status.httpCallback !== undefined ) {
+	    	$scope.status.ft.pageInfo.currentPage = $scope.status.pageNumber;
+	    	$scope.status.httpCallback($scope.status.ft, $scope.status.pageNumber);
+	    	$scope.status.httpCallback = undefined;
+	    	$scope.status.ft = undefined;
+	    	$scope.status.pageNumber = undefined;
+	    }
+
+		$scope.loadingRefresh = false;
+
 	}
 
 	return vm;
