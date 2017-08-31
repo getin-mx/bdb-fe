13f5d06a1d727c1c5dd672eff488fd47831222b7
Matias Hapanowicz <mhapanow@hotmail.com>
2017-08-30 19:24:51 -0500

    Data

--- app/scripts/custom/devicemessage.controller.js
+++ app/scripts/custom/devicemessage.controller.js
@@ -5,23 +5,74 @@ function DeviceMessageCtrl($scope, $http, $location, CommonsService, Authenticat
 
 	var vm = this;
 
+	$scope.types = null; 	
+	$scope.type = null;
+
+	$scope.typeDevice = 'hidden';
+	$scope.typeUser = 'hidden';
+
+
 	$scope.init = function() {
 
+		$scope.types = Array();
+		var type = null;
+		type = {
+			id: 'user',
+			name: 'Por Usuario'
+		}
+		$scope.type = type;
+		$scope.types.push(type);
+		type = {
+			id: 'device',
+			name: 'Por Dispositivo'
+		}
+		$scope.types.push(type);
+		type = {
+			id: 'all',
+			name: 'Masivo (Usar bajo tu propio riesgo)'
+		}
+		$scope.types.push(type);
+		$scope.typeChange();
 	}
 
-	$scope.postInit = function(data) {
+	$scope.typeChange = function() {
 
+		if( $scope.type.id === 'user') {
+			$scope.typeUser = '';
+			$scope.typeDevice = 'hidden';
+		} else if( $scope.type.id === 'device') {
+			$scope.typeUser = 'hidden';
+			$scope.typeDevice = '';
+		} else {
+			$scope.typeUser = 'hidden';
+			$scope.typeDevice = 'hidden';
+		}
 
 	}
 
 	$scope.update = function() {
 		$scope.loadingUpdate = true;
 
-		$scope.obj = {
-			deviceUUID: $scope.deviceUUID,
-			title: $scope.title,
-			message: $scope.message,
-			url: $scope.url
+		if( $scope.type.id === 'user') {
+			$scope.obj = {
+				userId: $scope.userId,
+				title: $scope.title,
+				message: $scope.message,
+				url: $scope.url
+			}
+		} else if( $scope.type.id === 'device') {
+			$scope.obj = {
+				deviceUUID: $scope.deviceUUID,
+				title: $scope.title,
+				message: $scope.message,
+				url: $scope.url
+			}
+		} else {
+			$scope.obj = {
+				title: $scope.title,
+				message: $scope.message,
+				url: $scope.url
+			}
 		}
 
 		$http.post(CommonsService.getDashUrl('/deviceMessage'), $scope.obj)
