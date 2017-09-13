ce9e59741df3b6af5dc4c6f8f55feef837eb640d
Luis Alberto Cruz <luicruz.amaya@gmail.com>
2017-09-12 19:20:25 -0500

    Merge branch 'Adstaging'

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -8,13 +8,13 @@
  */
 var config = {};
 
-// config.baseUrl = 'http://api.allshoppings.mobi/bdb';
-// config.dashUrl = 'http://api.allshoppings.mobi/appv2';
-// config.uploadUrl = 'http://api.allshoppings.mobi/bdb/img/upload'
+config.baseUrl = 'http://api.allshoppings.mobi/bdb';
+config.dashUrl = 'http://api.allshoppings.mobi/appv2';
+config.uploadUrl = 'http://api.allshoppings.mobi/bdb/img/upload'
 
-config.baseUrl = 'http://staging.getin.mx/bdb';
-config.dashUrl = 'http://staging.getin.mx/appv2';
-config.uploadUrl = 'http://staging.getin.mx/bdb/img/upload'
+// config.baseUrl = 'http://staging.getin.mx/bdb';
+// config.dashUrl = 'http://staging.getin.mx/appv2';
+// config.uploadUrl = 'http://staging.getin.mx/bdb/img/upload'
 
 // config.baseUrl = 'http://localhost:8081/bdb';
 // config.dashUrl = 'http://localhost:8081/appv2';
