865bed695a43028863036877fcd75d787529506a
Fernando Jimenez <lecopain@gmail.com>
2017-10-11 14:31:45 -0500

    updates on visitor and gauges graphs

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -7,18 +7,18 @@
  * General configuration
  */
 var config = {};
-
+//
 // config.baseUrl = 'http://api.allshoppings.mobi/bdb';
 // config.dashUrl = 'http://api.allshoppings.mobi/appv2';
 // config.uploadUrl = 'http://api.allshoppings.mobi/bdb/img/upload'
 
-config.baseUrl = 'http://staging.getin.mx/bdb';
-config.dashUrl = 'http://staging.getin.mx/appv2';
-config.uploadUrl = 'http://staging.getin.mx/bdb/img/upload'
+// config.baseUrl = 'http://staging.getin.mx/bdb';
+// config.dashUrl = 'http://staging.getin.mx/appv2';
+// config.uploadUrl = 'http://staging.getin.mx/bdb/img/upload'
 
-// config.baseUrl = 'http://localhost:8081/bdb';
-// config.dashUrl = 'http://localhost:8081/appv2';
-// config.uploadUrl = 'http://localhost:8081/bdb/img/upload'
+config.baseUrl = 'http://localhost:8081/bdb';
+config.dashUrl = 'http://localhost:8081/appv2';
+config.uploadUrl = 'http://localhost:8081/bdb/img/upload'
 
 config.oneMonth = 2505600000;
 config.oneWeek = 518400000;
