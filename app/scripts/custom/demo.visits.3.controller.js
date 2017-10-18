088e24193c3dd4f0aa50c782f6ae654e8f1b0e90
Fernando Jimenez <lecopain@gmail.com>
2017-10-18 12:35:34 -0500

    small demo corrections

--- app/scripts/custom/demo.visits.3.controller.js
+++ app/scripts/custom/demo.visits.3.controller.js
@@ -57,7 +57,6 @@ function DemoVisits3Ctrl($rootScope, $scope, AuthenticationService, CommonsServi
     }
 
     this.updateStoreList = function(id, baseUrl, entityId) {
-        debugger;
         $.getJSON(
             baseUrl
             + '/dashboard/storesFilter?entityId=' + entityId
@@ -555,6 +554,8 @@ function DemoVisits3Ctrl($rootScope, $scope, AuthenticationService, CommonsServi
             + '&onlyExternalIds=true'
             + '&timestamp=' + CommonsService.getTimestamp(),
             function(data) {
+                var sum = 0;
+                var val = 0;
                 var tab = '';
                 tab = '<table class="table table-striped" style="text-align: center;" >';
                 tab += '<tr style="font-weight:bold;">';
@@ -562,9 +563,10 @@ function DemoVisits3Ctrl($rootScope, $scope, AuthenticationService, CommonsServi
                 tab += '<td>Paseantes</td>';
                 tab += '<td>Visitantes</td>';
                 tab += '<td>Tickets</td>';
+                tab += '<td>Items</td>';
                 tab += '<td>Ventas</td>';
-                tab += '<td>Paseantes/Visitantes</td>';
-                tab += '<td>Visitantes/Tickets</td>';
+                tab += '<td>Visitas/Paseantes</td>';
+                tab += '<td>Tickets/Visitas</td>';
                 tab += '<td>Día más Alto</td>';
                 tab += '<td>Día más Bajo</td>';
                 tab += '<td>Permanencia Promedio</td>';
@@ -573,14 +575,22 @@ function DemoVisits3Ctrl($rootScope, $scope, AuthenticationService, CommonsServi
                 for (var i = 1; i < data.length - 1; i++) {
                     tab += '<tr>';
                     for (var x = 0; x < data[i].length; x++) {
+                        val = 0;
                         data[i][x] = data[i][x].replace('Flormar Altaplaza', 'Tienda 1');
                         data[i][x] = data[i][x].replace('Flormar Metromall', 'Tienda 2');
                         data[i][x] = data[i][x].replace('Flormar Multicentro', 'Tienda 3');
                         data[i][x] = data[i][x].replace('Flormar Multiplaza', 'Tienda 4');
-                        if (x == 0 || x == 4 || x == 6 || x == 8)
+                        if (x == 0 || x == 4 || x == 6 || x == 8){
                             tab += '<td style="border-right: 1px solid gray;">' + data[i][x] + '</td>';
-                        else
+                        }
+                        else if(x == 10){
+                            val += (5 + Math.floor((Math.random() * 10) + 1));
+                            sum += val;
+                            tab += '<td>' + val + ' mins</td>';
+                        }
+                        else{
                             tab += '<td>' + data[i][x] + '</td>';
+                        }
                     }
                     tab += '</tr>';
                 }
@@ -588,8 +598,12 @@ function DemoVisits3Ctrl($rootScope, $scope, AuthenticationService, CommonsServi
                 for (var x = 0; x < data[data.length - 1].length; x++) {
                     if (x == 0 || x == 4 || x == 6 || x == 8)
                         tab += '<td style="border-right: 1px solid gray;">' + data[data.length - 1][x] + '</td>';
-                    else
+                    else if(x == 10){
+                        tab += '<td>' + sum + ' mins</td>';
+                    } else{
                         tab += '<td>' + data[data.length - 1][x] + '</td>';
+                    }
+
                 }
                 tab += '</tr></tbody></table>';
 
