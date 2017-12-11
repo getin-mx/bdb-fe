20348babda4fb13080c06b94718939538b78d476
Francisco Alvarez <FrankAlvarez@MacBook-Air-de-Francisco.local>
2017-12-11 13:06:54 -0600

    changed maintenance

--- app/scripts/custom/storetickets.controller.js
+++ app/scripts/custom/storetickets.controller.js
@@ -53,7 +53,7 @@ function StoreTicketsCtrl($scope, $http, $location, CommonsService, Authenticati
 		// validate token
 		if( data.status != 200 || data.data.error_code !== undefined )
 			AuthenticationService.logout(function(response) {
-				$location.path('/login');
+				$location.path('/loginAdmin');
 			});
 
 		for( var i = 0; i < data.data.data.length; i++ ) {
