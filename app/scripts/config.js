eb5fce9e9e5c629ce74805ec6a34eec7b96a773f
r2d2 Server <r2d2server@getin.mx>
2017-12-19 18:06:37 -0600

    changing to r2d2.getin.mx

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -16,9 +16,9 @@ var config = {};
 // config.dashUrl = 'http://staging.getin.mx/appv2';
 // config.uploadUrl = 'http://staging.getin.mx/bdb/img/upload'
 
-config.baseUrl = 'http://localhost:8081/bdb';
-config.dashUrl = 'http://localhost:8081/appv2';
-config.uploadUrl = 'http://localhost:8081/bdb/img/upload'
+config.baseUrl = 'http://r2d2.getin.mx/bdb';
+config.dashUrl = 'http://r2d2.getin.mx/appv2';
+config.uploadUrl = 'http://r2d2.getin.mx/bdb/img/upload'
 
 // config.baseUrl = 'http://r2d2.getin.mx/bdb';
 // config.dashUrl = 'http://r2d2.getin.mx/appv2';
