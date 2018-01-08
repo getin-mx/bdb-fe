06cc906d8a8f839e8d9f05f8ba08114789ba3a0f
Nachintoch <ignacio@getin.mx>
2018-01-08 13:41:23 -0600

    Added hardcoded brandTable removal for Modatelas

--- app/scripts/custom/apdvisits.controller.js
+++ app/scripts/custom/apdvisits.controller.js
@@ -1315,6 +1315,10 @@
     };
     this.updateBrandPerformanceTable = function(id, baseUrl, fromDate, toDate, entityId, storeType) {
         // TODO use store type
+        if(entityId == 'modatelas_mx') {
+            $('#brand-count').html('');
+            return;
+        }
         $http.get(CommonsService.getUrl('/dashboard/brandTableData')
             + '&entityId=' + entityId
             + '&entityKind=1'
